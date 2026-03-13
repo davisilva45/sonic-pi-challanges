f5b6d8415e196800d3da2d98e231b4209b3abdb5
davisilva45 <nigrodavi12@gmail.com>
2026-03-13 13:44:18 -0700

    Update my own song.rb

--- my own song.rb
+++ my own song.rb
@@ -3,29 +3,15 @@ use_bpm 93
 use_synth :piano
 live_loop :melodia do
   
-  play :c4
-  sleep 2
-  play :r
-  sleep 0.5
-  play :d4
-  sleep 0.5
-  play :f4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  
-  play :a4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :a4, release: 1.5
-  sleep 1.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :g4
-  sleep 0.5
+  notes = [:c4,:r,:d4,:f4,:g4,:a4,:b4,:a4,:b4,:b4,:g4]
+  times = [2,0.5,0.5,0.5,0.5,0.5,0.5,1.5,0.5,0.5,0.5]
+  X = 0
+  11.times do
+    play notes[X]
+    sleep times[X]
+    X = X + 1
+  end
+  sleep 5
   
   play :d4
   sleep 0.5
@@ -46,9 +32,15 @@ live_loop :melodia do
   sleep 0.5
 end
 
+sleep 8
+
 live_loop :drums do
   sample :drum_bass_hard
   sleep 1
   sample :drum_snare_hard
   sleep 1
 end
+sleep 7
+
+sample :loop_amen
+
